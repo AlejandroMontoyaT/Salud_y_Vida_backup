334ded91c8b4a64ce4ad6049eeb062cc8a6fcf3d
ISC.ALEJANDRO <isc.alejandro14@gmail.com>
2024-05-01 22:29:28 -0600

    para la url

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from "vite";
+import vue from '@vitejs/plugi-vue'
+
+export default defineConfig({
+    plugins: [vue()],
+    base:'https:///alejandromontoyat.github.io/Salud_y_Vida_backup/'
+})
