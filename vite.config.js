b2003866002dc5d2ab38af40144ba5374034a29f
Alejandro Montoya <isc.alejandro14@gmail.com>
2024-05-01 23:30:53 -0600

    borro archivo js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import vue from '@vitejs/plugi-vue'
-
-export default defineConfig({
-    plugins: [vue()],
-    base:'https:///alejandromontoyat.github.io/Salud_y_Vida_backup/'
-})
